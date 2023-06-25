51861eb118f2b530b69fe004e26bd06942dbc25f
Kustov Ilia <kustoff.il@yandex.ru>
2023-06-26 01:32:47 +0300

    Add changes_2

--- src/pages/chat-page/index.ts
+++ src/pages/chat-page/index.ts
@@ -92,10 +92,12 @@ class ChatPage extends Block {
   }
 
   createMessages() {
-    return createMessageList(
-      this.props.messageData as MessageDataType[],
-      (this.props.userData as UserData).id
-    );
+    if (this.props.userData) {
+      return createMessageList(
+        this.props.messageData as MessageDataType[],
+        (this.props.userData as UserData).id
+      );
+    } else return [];
   }
 
   createChats() {
