3d06c7570c16c0feb8d0a1051a0c24591579a5bc
Kustov Ilia <kustoff.il@yandex.ru>
2023-06-26 01:23:58 +0300

    Add changes

--- src/components/sidebar/chat-item/index.ts
+++ src/components/sidebar/chat-item/index.ts
@@ -1,17 +1,10 @@
 import Block, { BlockProps } from "../../../core/block";
-import Store from "../../../core/store";
 import { withStore } from "../../../utils/withStore";
 import template from "./chat-item.hbs";
 
 class ChatItem extends Block {
   constructor(props: BlockProps = {}) {
     super("ChatItem", props);
-  }
-
-  protected init(): void {
-    this.props.own =
-      this.props.created_by !=
-      (this.props.store as Store).getState().userData!.id;
   }
 
   protected render(): DocumentFragment {
